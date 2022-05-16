ea96f4f4f5cdf11e057ac7519e29f7d53ef9787a
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-05-16 22:01:28 +0300

    two way binding

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import React, { useState } from 'react';
 
 function App() {
   let [count, setCount] = useState(0)
+  let [value, setValue] = useState('Text')
   return (
-    <div className="App"><h1>{count}</h1>
+    <div className="App">
+      <h1>{count}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
       <button onClick={() => setCount(count += 1)}>Increment</button>
       <button onClick={() => setCount(count -= 1)}>Decrement</button>
     </div>
