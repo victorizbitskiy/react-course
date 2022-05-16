aed3b36ed230884f6e6bc7d41b4b4df3cbf65b77
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-05-16 21:29:22 +0300

    increment/decrement example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  let [count, setCount] = useState(0)
   return (
-    <div className="App">
-
+    <div className="App"><h1>{count}</h1>
+      <button onClick={() => setCount(count += 1)}>Increment</button>
+      <button onClick={() => setCount(count -= 1)}>Decrement</button>
     </div>
   );
 }
