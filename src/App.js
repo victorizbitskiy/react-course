e169a1b21077c721d333efa7b9509eb49549fe5b
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-05-17 20:38:46 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
+import Counter from './components/Counter'
 
 function App() {
-  let [count, setCount] = useState(0)
   let [value, setValue] = useState('Text')
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={() => setCount(count += 1)}>Increment</button>
-      <button onClick={() => setCount(count -= 1)}>Decrement</button>
+      <Counter />
     </div>
   );
 }
