c8253acc8d1bda061f49f2e4e679ad4331eab952
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-05-20 13:59:25 +0300

    two-way binding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import PostList from "./components/UI/PostList"
+import PostList from "./components/UI/PostList";
 import "./styles/App.css";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -12,12 +12,19 @@ function App() {
     { id: 4, title: "Java", body: "Description" },
   ]);
 
+  const [title, setTitle] = useState("");
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+  };
+
   return (
     <div className="App">
       <form>
-        <MyInput type="text" placeholder="Название поста"></MyInput>
+        {/* Управлемый компонент */}
+        <MyInput value={title} onChange={e => setTitle(e.target.value)} type="text" placeholder="Название поста" />
         <MyInput type="text" placeholder="Описание поста"></MyInput>
-        <MyButton>Создать пост</MyButton>
+        <MyButton type="submit" onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title="Список постов" />
     </div>
