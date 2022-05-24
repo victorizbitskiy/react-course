2b02f8a051c91bbde0f1eb6f48ecbc9b34df1e01
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-05-24 21:29:19 +0300

    add sort function

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import MySelect from "./components/UI/select/MySelect";
 
 function App() {
   let [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript", body: "Description" },
-    { id: 2, title: "Python", body: "Description" },
-    { id: 3, title: "ABAP", body: "Description" },
-    { id: 4, title: "Java", body: "Description" },
+    { id: 1, title: "JavaScript", body: "1 JavaScript Description" },
+    { id: 2, title: "Python", body: "10 Python Description" },
+    { id: 3, title: "ABAP", body: "5 ABAP Description" },
+    { id: 4, title: "Java", body: "8 Java Description" },
   ]);
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -20,18 +21,26 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+  const sortPosts = (sortType) => {
+    setSelectedSort(sortType);
+    setPosts([...posts].sort((a, b) => a[sortType].localeCompare(b[sortType])));
+  };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: "15px 0" }}></hr>
 
       <div>
-        <MySelect 
-        deafultValue="Сортировка по" 
-        options={[
-          {value: 'title', name: 'По названию'},
-          {value: 'names', name: 'По описанию'}]} 
-          />
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          deafultValue="Сортировка по"
+          options={[
+            { value: "title", name: "По названию" },
+            { value: "body", name: "По описанию" },
+          ]}
+        />
       </div>
 
       {posts.length ? (
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 // import classes from './MySelect.module.css';
 import classes from "./MySelect.module.css";
 
-const MySelect = ({ options, deafultValue }) => {
+const MySelect = ({ options, deafultValue, value, onChange }) => {
 
   return (
     <div >
-      <select className={classes.myInput}>
-        <option disabled>{deafultValue}</option>
+      <select 
+      className={classes.myInput}
+      value={value}
+      onChange={event => onChange(event.target.value)}
+      >
+        <option disabled value="">{deafultValue}</option>
         {options.map(option =>
           <option key={option.value} value={option.value}>
             {option.name}
