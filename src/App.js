abf8dc11e4319d0d4381ce2897a94fed0bbf0466
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-06-15 06:53:08 +0300

    routing part 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/UI/Navbar/Navbar";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
 import "./styles/App.css";
@@ -6,9 +7,13 @@ import "./styles/App.css";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<Posts />} />
         <Route path="about" element={<About />} />
         <Route path="posts" element={<Posts />} />
+        <Route path="*" element={<Navigate to="/" replace />}
+    />
       </Routes>
     </BrowserRouter>
   );
