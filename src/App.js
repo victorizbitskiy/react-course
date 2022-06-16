58b220773c3cd564a7beb6152e12e7c8a7efb3b5
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-06-16 05:50:53 +0300

    add error page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Error from "./pages/Error"
 import "./styles/App.css";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <Route path="/" element={<Posts />} />
         <Route path="about" element={<About />} />
         <Route path="posts" element={<Posts />} />
-        <Route path="*" element={<Navigate to="/" replace />}
+        <Route path="error" element={<Error />} />
+        <Route path="*" element={<Navigate to="error" replace />}
     />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <h1>
+      Что-то пошло не так...
+    </h1>
+  );
+};
+
+export default Error;
