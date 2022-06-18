748d605d500291559e5b54b7a66fe4bc2272fb43
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-06-18 07:07:54 +0300

    nav refactoring

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import Error from "./pages/Error"
-import PostIdPage from "./pages/PostIdPage";
 import "./styles/App.css";
+import { routes } from "./router/routes";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Posts />} />
-        <Route path="about" element={<About />} />
-        <Route path="posts" element={<Posts />} />
-        <Route path="posts/:id" element={<PostIdPage />} />
-        <Route path="error" element={<Error />} />
-        <Route path="*" element={<Navigate to="error" replace />}
-    />
+        {routes.map((route, index) => (
+          <Route
+            element={<route.element/>}
+            path={route.path}
+            exact={route.exact}
+            key={index}
+          />
+        ))}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/router/routes.js
@@ -0,0 +1,11 @@
+import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
+import Posts from "../pages/Posts";
+
+
+export const routes = [
+  { path: '/about', element: About, exact: true },
+  { path: '/posts', element: Posts, exact: true },
+  { path: '/posts/:id', element: PostIdPage, exact: true },
+  { path: '/error', element: Error, exact: true }
+]
