f28136f81418ecb5bac7a2e4cd06b02211f3b085
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-06-21 20:47:52 +0300

    work with localstorage and auth

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/UI/AppRouter";
@@ -6,9 +7,16 @@ import { AuthContext } from "./context";
 import "./styles/App.css";
 
 function App() {
-      const [isAuth, setIsAuth] = useState(false)
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("auth")) {
+      setIsAuth(true)
+    }
+  });
+
   return (
-    <AuthContext.Provider value={{isAuth, setIsAuth}}>
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
       <BrowserRouter>
         <Navbar />
         <AppRouter />
--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -5,9 +5,13 @@ import MyButton from '../button/MyButton';
 
 const Navbar = () => {
   const { isAuth, setIsAuth } = useContext(AuthContext);
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.removeItem('auth')
+  }
   return (
     <div className='navbar'>
-      <MyButton onClick={() => setIsAuth(false)}>
+      <MyButton onClick={logout}>
         Выйти
       </MyButton>
       <div className='navbar__links'>
